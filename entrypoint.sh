66f4b156cef3870e3b7acbff872a8a123329941f
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-16 01:36:15 +0700

    fix: add `-U` to avoid fatal error

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ docker-entrypoint.sh postgres &
 
 env | grep '^POSTGRES_' | sed 's/^POSTGRES_/PG/' > /var/app/.env
 
-until pg_isready -q; do sleep 1; done
+until pg_isready -q -U "$POSTGRES_USER"; do sleep 1; done
 exec /bin/app
