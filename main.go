3bcf49159cdbc61e6e28154703434a4596775fbb
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-15 13:30:42 +0700

    feat: add

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
@@ -52,40 +54,13 @@ loop:
 			continue
 		}
 
+		fmt.Println()
+
 		switch selection {
 		case 1:
-			rows, err := conn.Query(context.Background(), `SELECT * FROM "contacts"`)
-			if err != nil {
-				log.Fatal("Failed at querying", err)
-			}
-
-			entries, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Contact])
-			if err != nil {
-				log.Fatal("Failed at collecting", err)
-			}
-
-			for index, entry := range entries {
-				g, _ := goment.New(entry.UpdatedAt)
-
-				fmt.Printf("%d. (Last updated: %v)\n", index, g.FromNow())
-				fmt.Printf("Name: %v\n", entry.Name)
-				if entry.Dob != nil {
-					fmt.Printf("Dob: %v\n", entry.Dob.Format("2006-01-02"))
-				}
-				if entry.Address != nil {
-					fmt.Printf("Address: %v\n", *entry.Address)
-				}
-				if entry.Phone != nil {
-					fmt.Printf("Phone: %v\n", *entry.Phone)
-				}
-				if entry.Email != nil {
-					fmt.Printf("Email: %v\n", *entry.Email)
-				}
-				fmt.Println()
-			}
-
-			fmt.Print("Enter to continue... ")
-			scanner.Scan()
+			list(conn, scanner)
+		case 2:
+			add(conn, scanner)
 
 		case 0:
 			break loop
@@ -95,5 +70,120 @@ loop:
 	err = scanner.Err()
 	if err != nil {
 		log.Fatal("Buffer error", err)
+	}
+}
+
+func list(conn *pgx.Conn, scanner *bufio.Scanner) {
+	rows, err := conn.Query(context.Background(), `SELECT * FROM "contacts"`)
+	if err != nil {
+		log.Fatal("Failed at querying", err)
+	}
+
+	entries, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Contact])
+	if err != nil {
+		log.Fatal("Failed at collecting", err)
+	}
+
+	for index, entry := range entries {
+		g, _ := goment.New(entry.UpdatedAt)
+
+		fmt.Printf("%d. (Last updated: %v)\n", index, g.FromNow())
+		fmt.Printf("Name: %v\n", entry.Name)
+		if entry.Dob != nil {
+			fmt.Printf("Dob: %v\n", entry.Dob.Format("2006-01-02"))
+		}
+		if entry.Address != nil {
+			fmt.Printf("Address: %v\n", *entry.Address)
+		}
+		if entry.Phone != nil {
+			fmt.Printf("Phone: %v\n", *entry.Phone)
+		}
+		if entry.Email != nil {
+			fmt.Printf("Email: %v\n", *entry.Email)
+		}
+		fmt.Println()
+	}
+
+	fmt.Print("Enter to continue... ")
+	scanner.Scan()
+}
+
+func add(conn *pgx.Conn, scanner *bufio.Scanner) {
+	var (
+		name string
+		dob  *time.Time
+
+		address,
+		phone,
+		email *string
+	)
+
+	var input string
+
+	fmt.Print("Name: ")
+	scanner.Scan()
+	input = strings.TrimSpace(scanner.Text())
+	name = input
+
+	for {
+		fmt.Print("Dob (2006-01-02): ")
+		scanner.Scan()
+		input = strings.TrimSpace(scanner.Text())
+		if input == "" {
+			break
+		}
+
+		time, err := time.Parse("2006-01-02", input)
+		if err != nil {
+			fmt.Fprint(os.Stderr, "Failed parse", err)
+			continue
+		}
+
+		dob = &time
+		break
+	}
+
+	fmt.Print("Address: ")
+	scanner.Scan()
+	input = strings.TrimSpace(scanner.Text())
+	if input != "" {
+		value := input
+		address = &value
+	}
+
+	fmt.Print("Phone: ")
+	scanner.Scan()
+	input = strings.TrimSpace(scanner.Text())
+	if input != "" {
+		value := input
+		phone = &value
+	}
+
+	fmt.Print("Email: ")
+	scanner.Scan()
+	input = strings.TrimSpace(scanner.Text())
+	if input != "" {
+		value := input
+		email = &value
+	}
+
+	fmt.Println()
+
+	args := pgx.NamedArgs{
+		"name":    name,
+		"dob":     dob,
+		"address": address,
+		"phone":   phone,
+		"email":   email,
+	}
+
+	_, err := conn.Exec(
+		context.Background(),
+		`INSERT INTO "contacts" ("name", "dob", "address", "phone", "email") `+
+			`VALUES (@name, @dob, @address, @phone, @email)`,
+		args,
+	)
+	if err != nil {
+		log.Fatal("Failed at executing", err)
 	}
 }
